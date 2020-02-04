30be21c415d58ceceb40d6e529531398d3682b29
Chuku <Viberos993@outlook.com>
2020-02-04 09:43:55 -0300

    Ajustes para poder usar el VisualStudioCode

---
+++ Assets/Scripts/CursorObjeto.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorObjeto : MonoBehaviour
+{
+    public Texture2D CursorNormal;
+    //public Texture2D CursorEnter;
+    //public Texture2D CursorExit;
+
+    void Start() {
+        Cursor.SetCursor(CursorNormal, Vector2.zero, CursorMode.ForceSoftware);
+    }
+    /*
+    void OnMouseEnter() {
+        Cursor.SetCursor(CursorEnter, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    void OnMouseExit() {
+        Cursor.SetCursor(CursorExit, Vector2.zero, CursorMode.ForceSoftware);
+    }
+    */
+}
---
+++ Assets/Scripts/ManagerHumano.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerHumano : MonoBehaviour
+{
+    public int poblacion;
+    public int Recursos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("poblacion" + poblacion);
+    }
+}
